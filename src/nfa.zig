71cd30c1eba0c8f693d8623e4005bbca10500dc1
Asahela <geekkoloina@gmail.com>
2025-07-23 01:06:23 -0400

    refactor: move regular operation's function handler

--- src/nfa.zig
+++ src/nfa.zig
@@ -1,8 +1,5 @@
 const std = @import("std");
 const State = @import("state.zig").State;
-const builder = @import("nfa_builder.zig");
-const Symbol = @import("symbol.zig").Symbol;
-const SymbolTag = @import("symbol.zig").SymbolTag;
 
 pub const Nfa = struct {
     in: *State,
@@ -20,6 +17,80 @@ pub const Nfa = struct {
     }
 };
 
+pub fn epsilon(allocator: std.mem.Allocator) !*Nfa {
+    return try char(.{ .epsilon = {} }, allocator);
+}
+
+pub fn char(character: u8, allocator: std.mem.Allocator) !*Nfa {
+    const in_state = try allocator.create(State);
+    const out_state = try allocator.create(State);
+    in_state.* = State.init(allocator);
+    out_state.* = State.init(allocator);
+
+    out_state.is_accepting = true;
+
+    try in_state.addTransition(.{ .alphabet = character }, out_state);
+
+    const nfa = try allocator.create(Nfa);
+    nfa.* = Nfa.create(in_state, out_state);
+    return nfa;
+}
+
+pub fn repetition(nfa: *Nfa) !*Nfa {
+    try nfa.in.addTransition(.{ .epsilon = {} }, nfa.out);
+    try nfa.out.addTransition(.{ .epsilon = {} }, nfa.in);
+
+    return nfa;
+}
+
+pub fn alternationPair(first: *Nfa, second: *Nfa, allocator: std.mem.Allocator) !*Nfa {
+    first.out.is_accepting = false;
+    second.out.is_accepting = true;
+
+    try first.out.addTransition(.{ .epsilon = {} }, second.in);
+
+    const nfa = try allocator.create(Nfa);
+    nfa.* = Nfa.create(first.in, second.out);
+    return nfa;
+}
+
+pub fn alternation(first: *Nfa, secondaries: []*Nfa, allocator: std.mem.Allocator) !*Nfa {
+    var nfa: *Nfa = first;
+    for (secondaries) |secondary| {
+        nfa = try alternationPair(nfa, secondary, allocator);
+    }
+    return nfa;
+}
+
+pub fn disjunctionPair(first: *Nfa, second: *Nfa, allocator: std.mem.Allocator) !*Nfa {
+    first.out.is_accepting = false;
+    second.out.is_accepting = false;
+
+    const in_state = try allocator.create(State);
+    const out_state = try allocator.create(State);
+    in_state.* = State.init(allocator);
+    out_state.* = State.init(allocator);
+
+    out_state.is_accepting = true;
+
+    try in_state.addTransition(.{ .epsilon = {} }, first.in);
+    try in_state.addTransition(.{ .epsilon = {} }, second.in);
+    try first.out.addTransition(.{ .epsilon = {} }, out_state);
+    try second.out.addTransition(.{ .epsilon = {} }, out_state);
+
+    const nfa = try allocator.create(Nfa);
+    nfa.* = Nfa.create(in_state, out_state);
+    return nfa;
+}
+
+pub fn disjunction(first: *Nfa, secondaries: []*Nfa, allocator: std.mem.Allocator) !*Nfa {
+    var nfa = first;
+    for (secondaries) |secondary| {
+        nfa = try disjunctionPair(nfa, secondary, allocator);
+    }
+    return nfa;
+}
+
 test "matches" {
     const abc = "abc";
     const dddd = "dddd";
@@ -29,18 +100,18 @@ test "matches" {
     var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
     defer arena.deinit();
 
-    var abcOrdddd = try builder.orOp(
-        try builder.concatOp(
-            try builder.char('a', arena.allocator()),
+    var abcOrdddd = try disjunction(
+        try alternation(
+            try char('a', arena.allocator()),
             @constCast(&[_]*Nfa{
-                try builder.char('b', arena.allocator()),
-                try builder.char('c', arena.allocator()),
+                try char('b', arena.allocator()),
+                try char('c', arena.allocator()),
             }),
             arena.allocator(),
         ),
         @constCast(
             &[_]*Nfa{
-                try builder.repOp(try builder.char('d', arena.allocator())),
+                try repetition(try char('d', arena.allocator())),
             },
         ),
         arena.allocator(),
